37a7a9b5e613b498ea0d9f719dada71f0b4c7999
Nicole <125073453+lemonadelightice@users.noreply.github.com>
2024-05-24 12:48:09 -0400

    styling and fumctionality updates for nav bar, message section,  and projects

--- js/index.js
+++ js/index.js
@@ -1,29 +1,77 @@
+//add functionality to nav bar
+document.addEventListener("DOMContentLoaded", function() {
+  const navLinks = document.querySelectorAll('#navigation a');
 
+  navLinks.forEach(link => {
+      link.addEventListener('click', function(event) {
+          event.preventDefault();
+          const targetId = this.getAttribute('href');
+          const targetSection = document.querySelector(targetId);
+          targetSection.scrollIntoView({ behavior: 'smooth' });
+      });
+  });
+});
 
-const currentDate = new Date();
-const thisYear = currentDate.getFullYear();
+//Skills section
 
-const footer = document.createElement('footer');
-const copyright = document.createElement('p');
-copyright.innerHTML = `<small>Nicole Jordan &copy; ${thisYear}</small>`;
+let skills = ["JavaScript", "HTML", "CSS", "AWS Cloud"];
+let skillsContainer = document.getElementById("skills-container");
+console.log("Skills Container:", skillsContainer);
 
-footer.appendChild(copyright);
-document.body.appendChild(footer);
-
-//const footer = document.querySelector('footer');
-
-let skills = ["AWS Cloud", "JavaScript", "CSS", "HTML", "CSS", "JSON"];
-let skillsSect = document.getElementById("skills");
-let skillsList = document.createElement("ul");
-skillsSect.appendChild(skillsList);
-
-for (let skill of skills) {
-    let skillItem = document.createElement("li");
-    skillItem.innerText = skill;
-    skillsList.appendChild(skillItem);
+for(let skill of skills) {
+  let skillItem = document.createElement("div");
+  skillItem.className = "skill-item";
+  skillItem.innerText = skill;
+  skillsContainer.appendChild(skillItem);
 }
 
-let messageForm = document.querySelector("[name='leave_message']");
+//create a fetch for github repos for projects
+const userName = "lemonadelightice";
+
+fetch(`https://api.github.com/users/${userName}/repos`)   
+
+ .then((response) => {
+    if (response.ok) {
+      return response.text();
+      } else {
+        throw new Error("Failed to fetch repositories");
+      }
+    })
+  .then((data) => {
+    const repositories = JSON.parse(data);
+    console.log(repositories);
+ 
+  //DOM to select Projects section by ID
+  const projectSection = document.getElementById("projects");
+
+  //Create ul in projects section
+  let projectList = document.createElement("ul");
+  projectList.classList.add("project-list");
+  projectSection.appendChild(projectList);
+
+    for (let repository of repositories) {
+    let project = document.createElement("li");
+    project.classList.add("project-item");
+    let link = document.createElement("a");
+    link.href = repository.html_url;
+    link.textContent = repository.name;
+    link.classList.add("button-link");
+    project.appendChild(link);
+    projectList.appendChild(project);
+  }
+  })
+
+ .catch((error) => {
+    if (error instanceof SyntaxError) {
+        console.error("Unparsable response from server");
+    } else {
+        console.error("Error fetching data: ", error.message);
+    }
+});
+
+
+//Create Message Section
+let messageForm = document.querySelector("[name='leave-message']");
 let messageSection = document.getElementById('message-section');
 let messageList = messageSection.querySelector('ul');
 messageSection.hidden = true;
@@ -81,45 +129,16 @@ function makeRemoveButton() {
     return removeButton;
 };
 
-//create a fetch for github repos
-const userName = "lemonadelightice";
+//Footer
+const currentDate = new Date();
+const thisYear = currentDate.getFullYear();
 
-fetch(`https://api.github.com/users/${userName}/repos`)
- .then((response) => {
-    if (response.ok) {
-      return response.text();
-      } else {
-        throw new Error("Failed to fetch repositories");
-      }
-    })
-  .then((data) => {
-    const repositories = JSON.parse(data);
-    console.log(repositories);
- 
-  //DOM to select Projects section by ID
-  const projectSection = document.getElementById("projects");
+const footer = document.createElement('footer');
+const copyright = document.createElement('p');
+copyright.innerHTML = `<small>Nicole Jordan &copy; ${thisYear}</small>`;
 
-  //Create ul in projects section
-  let projectList = document.createElement("ul");
-  projectSection.appendChild(projectList);
-
-  for (let repository of repositories) {
-    let project = document.createElement("li");
-    project.innerText = repository.name;
-    projectList.appendChild(project);
-  }
-})
- .catch((error) => {
-    if (error instanceof SyntaxError) {
-        console.error("Unparsable response from server");
-    } else {
-        console.error("Error fetching data: ", error.message);
-    }
-});
-
-
-
-
+footer.appendChild(copyright);
+document.body.appendChild(footer);
 
 
 
